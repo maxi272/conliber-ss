bc0a9c47fac2f4e7d8b6495c27d2bf23e464522a
maxi272 <84964924+maxi272@users.noreply.github.com>
2021-05-28 11:33:56 +0200

    Add files via upload

---
+++ step4.php
@@ -0,0 +1,167 @@
+
+<html>
+<head>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+<!--navbar-->
+<nav class="bg-gray-600">
+    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div class="relative flex items-center justify-between h-16">
+            <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div class="flex-shrink-0 flex items-center">
+                    <img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">
+                    <img class="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
+                </div>
+                <div class="hidden sm:block sm:ml-6">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</nav>
+
+
+<!--stepper-->
+<div class=" stepper container mx-auto px-40">
+    <div class="stepper p-5">
+        <div class=" stepper mx-4 p-4">
+            <div class=" stepper flex items-center">
+                <div class=" stepper flex items-center text-indigo-600 relative">
+                    <div class=" stepper rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 border-indigo-600 bg-white">
+                        <svg xmlns="http://www.w3.org/2000/svg"  width="100%" height="100%"  fill="none" viewBox="0 0 24 24" stroke="currentColor" class="feather feather-key">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z" />
+                        </svg>
+
+                    </div>
+                    <div class=" absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-indigo-600">Reg-Code</div>
+                </div>
+                <div class="flex-auto border-t-2 transition duration-500 ease-in-out border-indigo-600"></div>
+                <div class="flex items-center text-indigo-600 relative">
+                    <div class="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 bg-white border-indigo-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                        </svg>
+                    </div>
+                    <div class="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-indigo-600">KGR-User</div>
+                </div>
+                <div class="flex-auto border-t-2 transition duration-500 ease-in-out border-indigo-600"></div>
+                <div class="flex items-center text-white relative">
+                    <div class="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 border-indigo-600 bg-indigo-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4" />
+                        </svg>
+                    </div>
+                    <div class="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-indigo-600">confirm</div>
+                </div>
+                <div class="flex-auto border-t-2 transition duration-500 ease-in-out border-gray-300"></div>
+                <div class="flex items-center text-gray-500 relative">
+                    <div class="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 border-gray-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1z" />
+                        </svg>
+                    </div>
+                    <div class="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-gray-500">Add-RFID</div>
+                </div>
+                <div class="flex-auto border-t-2 transition duration-500 ease-in-out border-gray-300"></div>
+                <div class="flex items-center text-gray-500 relative">
+                    <div class="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 border-gray-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="100%" width="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                        </svg>
+                    </div>
+                    <div class="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-gray-500">ready</div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+<!--    Buttons-->
+<div class="h-48 flex flex-wrap content-center justify-center text-center">
+
+    <div class="mt-8 p-4" style="padding-top:16% ">
+        <div class=" flex  justify-center bg-gray-50  sm:px-6 lg:px-8">
+            <div class="max-w-lg w-full space-y-5">
+                        <div class='flex space-x-2 w-full bg-white shadow-md rounded-lg overflow-hidden mx-auto'>
+                            <div class="card  content-end min-w-sm border border-gray-100 bg-white   transition-shadow shadow-xl hover:shadow-xl min-w-max">
+                                <!---->
+                                <div class="  card__media--aside "></div>
+                                <div class="flex items-center p-4">
+                                    <div class=" flex flex-col items-center">
+                                        <div class="h-20 w-20 md rounded-full relative flex  text-gray-500 min-w-max  -top-16 flex text-white row-start-1 row-end-3 text-purple-650 ring-1 ring-white" style="margin-bottom: 20px; background-color: #EEF2FF">
+                                            <svg  class="content-center text-center items-center " xmlns="http://www.w3.org/2000/svg" width="100%" height="80%" fill="none" viewBox="0 0 24 24" stroke="currentColor" style="margin-top: 5px">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                                            </svg>
+
+                                        </div>
+                                        <div class="flex flex-col space-y-1  items-center w-full">
+                                            <span class="text-3xl text-gray-800 font-light" style="text-align: left">Bist das DU?</span>
+                                            <h1 class="text-lg text-gray-600">
+                                              Maximilian
+                                            </h1>
+                                            <h1 class="text-lg text-gray-600" style="margin-bottom: 40px">
+                                                Schiller
+                                            </h1>
+                                            <div class=" flex space-x-2">
+                                                <form method="post" style="margin-block-end: unset">
+                                                <button class="ConfBtn group relative  justify-center py-3 px-6 border border-transparent text-m font-medium rounded-md text-indigo-100 bg-indigo-400 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" style="margin-left: 0px; margin-bottom: unset">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                                    </svg>
+                                                </button>
+                                                <button class="ConfBtn group relative  justify-center py-3 px-6 border border-transparent text-m font-medium rounded-md text-purple-100 bg-purple-400 hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" style="margin-bottom: unset; margin-right: unset">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                                                    </svg>
+                                                </button>
+                                                </form>
+                                            </div>
+                                            <div
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <!---->
+                            </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                    <!-- <h1 class="text-left text-2xl border-2 border-purple-300" style="padding-left: 70px;padding-right: 70px; padding-top: 20px; padding-bottom: 20px">
+                        Maximilian
+                        <br>
+                        Schiller
+                        <br>
+                        schimaxi
+                    </h1>-->
+                </div>
+            </div>
+        </div>
+     </div>
+
+                </div>
+ <div class="h-48 flex flex-wrap content-end justify-center text-center">
+     <!--Nein BTN autofocus-
+                <button type="submit" class="ConfBtn group relative  justify-center py-4 px-6 border border-transparent text-m font-medium rounded-md text-indigo-100 bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+     Ja BTN
+                <button type="submit" class="ConfBtn group relative  justify-center py-4 px-6 border border-transparent text-m font-medium rounded-md text-purple-100 bg-purple-500 hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                    </svg>
+                </button>
+            </div>
+-->
+
+
+</body>
+</html>
---
+++ step6.php
@@ -0,0 +1,132 @@
+
+<html>
+<head>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+<!--navbar-->
+<nav class="bg-gray-600">
+    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div class="relative flex items-center justify-between h-16">
+            <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div class="flex-shrink-0 flex items-center">
+                    <img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">
+                    <img class="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
+                </div>
+                <div class="hidden sm:block sm:ml-6">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</nav>
+
+
+<!--stepper-->
+<div class=" stepper container mx-auto px-40">
+    <div class="stepper p-5">
+        <div class=" stepper mx-4 p-4">
+            <div class=" stepper flex items-center">
+                <div class=" stepper flex items-center text-indigo-600 relative">
+                    <div class=" stepper rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 border-indigo-600 bg-white">
+                        <svg xmlns="http://www.w3.org/2000/svg"  width="100%" height="100%"  fill="none" viewBox="0 0 24 24" stroke="currentColor" class="feather feather-key">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z" />
+                        </svg>
+
+                    </div>
+                    <div class=" absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-indigo-600">Reg-Code</div>
+                </div>
+                <div class="flex-auto border-t-2 transition duration-500 ease-in-out border-indigo-600"></div>
+                <div class="flex items-center text-indigo-600 relative">
+                    <div class="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 bg-white border-indigo-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                        </svg>
+                    </div>
+                    <div class="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-indigo-600">KGR-User</div>
+                </div>
+                <div class="flex-auto border-t-2 transition duration-500 ease-in-out border-indigo-600"></div>
+                <div class="flex items-center text-indigo-600 relative">
+                    <div class="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 border-indigo-600 bg-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4" />
+                        </svg>
+                    </div>
+                    <div class="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-indigo-600">confirm</div>
+                </div>
+                <div class="flex-auto border-t-2 transition duration-500 ease-in-out border-indigo-600"></div>
+                <div class="flex items-center text-indigo-600 relative">
+                    <div class="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 bg-white border-indigo-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1z" />
+                        </svg>
+                    </div>
+                    <div class="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-indigo-600">Add-RFID</div>
+                </div>
+                <div class="flex-auto border-t-2 transition duration-500 ease-in-out border-indigo-600"></div>
+                <div class="flex items-center text-white relative">
+                    <div class="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 bg-indigo-600 border-indigo-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="100%" width="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                        </svg>
+                    </div>
+                    <div class="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-indigo-600">ready</div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+<div class="h-48 flex flex-wrap content-center justify-center text-center">
+<div class=" middlePart middlePart2 flex-col justify-center text-center" style="padding-top: 18%">
+        <h1 class=" stepTitle stepTitle2 mt-6 text-center text-3xl font-light text-gray-900" ">
+        Dein Account wurde erfolgreich erstellt.
+        </h1>
+
+    <div class="flex-col justify-center text-left" style="padding-top: 20px">
+        <h1 class=" inline-flex items-center mt-6 text-left text-md font-light text-gray-900" style="text-align: right; margin-top: 10px">
+            <svg class="mr-3 inline-block w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M5 13l4 4L19 7" />
+            </svg>Beschrifte deine RFID-Card mit Vor- und Nachnamen.
+        </h1>
+        <br>
+        <h1 class=" inline-flex items-center mt-6 text-left text-md font-light text-gray-900" style="text-align: left; margin-top: 10px">
+            <svg class="mr-3 inline-block w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M5 13l4 4L19 7" />
+            </svg>Benutze immer nur DEINE Karte.
+        </h1>
+        <br>
+        <h1 class=" inline-flex items-center mt-6 text-left text-md font-light text-gray-900" style="text-align: left; margin-top: 10px">
+            <svg class="mr-3 inline-block w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M5 13l4 4L19 7" />
+            </svg>Bewahre deine Karte niemals in der Nähe elektromagnetischer Felder auf.
+        </h1>
+        <br>
+        <h1 class=" inline-flex items-center mt-6 text-left text-md font-semibold text-red-600" style="text-align: left; margin-top: 10px">
+            <svg class="mr-3 inline-block w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+            </svg>Wende dich bei Verlust deiner RFID-Card sofort an einen Mitarbeiter.
+        </h1>
+        <br>
+        <h1 class=" inline-flex items-center mt-6 text-left text-md font-semibold text-red-600" style="text-align: left; margin-top: 10px">
+            <svg class="mr-3 inline-block w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+            </svg> Die Kosten einer Ersatz-RFID-Card sind vom Endbenutzer zu tragen.
+        </h1>
+    </div>
+    <div class="flex-col justify-center text-right" style="padding-top: 10px" >
+        <br>
+        <form method="post">
+            <button type="submit" name="submitbutton" class="mt-3 focus:outline-none button button-primary  font-semibold text-md px-6 pt-3 pb-3 rounded bg-purple-500 hover:bg-purple-600 text-white" id="otpSubmit">Akzeptieren und Weiter</button>
+        </form>
+    </div>
+</div>
+</div>
+
+
+</body>
+</html>
